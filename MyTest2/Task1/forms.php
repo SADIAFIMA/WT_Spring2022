fbf1426f99086e0b2e6bea9f3e0655b441644107
FIMA <95241212+SADIAFIMA@users.noreply.github.com>
2022-02-09 12:50:45 +0600

    Add files via upload

---
+++ MyTest2/Task1/forms.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <form action="Task-2/Validation.php" method="post">
+                 
+                       First Name:
+                        
+                    <input type="text" ><br><br>
+                    
+                        Last Name:
+                    <input type="text" >
+                    <br><br>
+                    Age:
+                        <input type="Age"><br><br>
+                        Designation:
+                   
+                        <input type="radio" name="designation">Junior programmer
+                        <input type="radio" name="designation">Senior programmer
+                        <input type="radio" name="designation">Project Lead
+                        <br><br>
+                        Preffered Language:
+                    
+                        <input type="checkbox" JAVA>JAVA
+                        <input type="checkbox" PHP>PHP
+                        <input type="checkbox" C++>C++
+                        <br><br>
+                        E-mail:
+                    
+                        <input type="text">
+                        <br><br>
+                        Password:
+                  
+                        <input type="password">
+                        <br><br>
+                        Please choose a a file
+                    
+                        <input type="submit" name="choose file" value="choose file">
+                        no file chosen
+                        <br><br>
+                        <input type="submit" value="Submit">
+                        <input type="reset" value="Reset">
+                 
+    </body>
+</html>
